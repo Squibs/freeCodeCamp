d522c8c4a33456085e748ec3c1d2cfcbc15c0f76
Squibs <z.r.holman+github@gmail.com>
2022-11-18 01:30:20 -0700

    Completed personal library project.

---
+++ Quality Assurance/personal-library/README.md
@@ -0,0 +1,15 @@
+# Personal Library
+
+<p align="center"><img src="/Images/screenshots/screenshot" height="400" alt="Screenshot of my Personal Library project."/></p>
+
+<em>Completed November 18, 2022</em>
+
+### [Personal Library](https://personal-library.sulph.repl.co/) (Click to view functional site)
+
+[Link to files on replit](https://replit.com/@Sulph/personal-library#tests/2_functional-tests.js)
+
+This project is very comparable to the previous projects. In this project we have to handle the routes `/api/book` which returns a list of all the books and the comments for that book and the route `api/book/{book_id}` which retrieves a single book and it's comments. There is not a whole lot to say about this project that would be any different from the previous two.
+
+I made use of the `deleteMany` model method in this project. This can find matching database objects with the given parameters, then deletes any that match. This can be used to delete all comments that are specific to one book, if that one book is to be deleted. I also used it for the `delete all books...` button, to remove all books and again all comments when the button is pressed.
+
+Like the previous challenge I had to end up converting the `mongoose` objects into normal JavaScript objects. This time I used `JSON.parse(JSON.stringify(data))` to do the conversion several times, as there are many of the `mongoose` objects returned in an array. `toObject()` or `toJSON()` would be less helpful in this situation, and stringifying and parsing the data seemed like a much better solution compared to iterating over the array and converting each object.
---
+++ Quality Assurance/personal-library/routes/api.js
@@ -0,0 +1,160 @@
+/*
+*
+*
+*       Complete the API routing below
+*       
+*       
+*/
+
+'use strict';
+const mongoose = require('mongoose');
+
+// mongo configuration
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+console.log(`mongoose readyState: ${mongoose.connection.readyState}`);
+
+const bookSchema = new mongoose.Schema({
+  title: { type: String, requried: true },
+});
+
+const commentSchema = new mongoose.Schema({
+  comment: { type: String, requried: true },
+  book_id: { type: String, requried: true },
+});
+
+const Book = mongoose.model('Book', bookSchema);
+const Comment = mongoose.model('Comment', commentSchema);
+
+module.exports = function (app) {
+
+  app.route('/api/books')
+    
+    //response will be array of book objects
+    //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
+    .get(function (req, res){
+      Comment.find({}, (err, foundComments) => {
+        if (err) return res.json({ error: err });
+
+        Book.find({}, (err, foundBooks) => {
+          if (err) return res.json({ error: err });
+
+          // convert array of mongoose objects to array of normal js objects
+          // .toObject() or .toJSON() doesn't work here because it is an array, instead of a single object
+          const booksWithComments = JSON.parse(JSON.stringify(foundBooks));
+          const comments = JSON.parse(JSON.stringify(foundComments));
+
+          booksWithComments.forEach((book) => {
+            book.comments = []; // add array of comments to each book
+            comments.forEach((comment) => {
+              if (comment.book_id === book._id) {
+                book.comments.push(comment.comment);
+              }
+            });
+            book.commentcount = comments.length;
+          });
+
+          return res.json(booksWithComments);
+        });
+      });
+    })
+
+    
+    //response will contain new book object including atleast _id and title
+    .post(function (req, res){
+      if (!req.body.title) return res.send('missing required field title');
+
+      Book.create({ title: req.body.title }, (err, createdBook) => {
+        if (err) return res.json({ error: err });
+        return res.json(createdBook);
+      });
+    })
+
+    
+    //if successful response will be 'complete delete successful'
+    .delete(function(req, res){
+      Book.deleteMany({}, (err) => {
+        if (err) return res.json({ error: err });
+
+        Comment.deleteMany({}, (err) => {
+          if (err) return res.json({ error: err });
+          return res.send('complete delete successful');
+        });
+      });
+    });
+
+
+
+  
+  app.route('/api/books/:id')
+    //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
+    .get(function (req, res){
+      Comment.find({ book_id: req.params.id }, (err, foundComments) => {
+        if (err) return res.json({ error: err });
+        
+        Book.findById(req.params.id, (err, foundBook) => {
+          if (err || !foundBook) return res.send('no book exists');
+
+          // convert array of mongoose objects to array of normal js objects
+          // .toObject() or .toJSON() doesn't work here because it is an array, instead of a single object
+          const bookWithComments = JSON.parse(JSON.stringify(foundBook));
+          const comments = JSON.parse(JSON.stringify(foundComments));
+          bookWithComments.comments = [];
+          
+          comments.forEach((comment) => {
+            bookWithComments.comments.push(comment.comment);
+          });
+
+          bookWithComments.commentcount = comments.length;
+
+          return res.json(bookWithComments);
+        })
+      });
+    })
+
+    
+    //json res format same as .get
+    .post(function(req, res){
+      if (!req.body.comment) return res.send('missing required field comment');
+      if (!req.params.id) return res.send('missing required field BookId');
+
+      Book.findById(req.params.id, (err, foundBook) => {
+        if (err || !foundBook) return res.send('no book exists');
+
+        Comment.create({ comment: req.body.comment, book_id: req.params.id }, (err, createdComment) => {
+          if (err) return res.json({ error: err });
+
+          Comment.find({ book_id: req.params.id }, (err, foundComments) => {
+            if (err) return res.json({ error: err });
+
+            // convert array of mongoose objects to array of normal js objects
+            // .toObject() or .toJSON() doesn't work here because it is an array, instead of a single object
+            const bookWithComments = JSON.parse(JSON.stringify(foundBook));
+            const comments = JSON.parse(JSON.stringify(foundComments));
+            bookWithComments.comments = [];
+
+            comments.forEach((comment) => {
+              bookWithComments.comments.push(comment.comment);
+            });
+
+            bookWithComments.commentcount = comments.length;
+
+            return res.json(bookWithComments);
+          });
+        });
+      });
+    })
+
+    
+    //if successful response will be 'delete successful'
+    .delete(function(req, res){
+      let bookid = req.params.id;
+      Book.findByIdAndDelete(req.params.id, (err, deletedBook) => {
+        if (err || !deletedBook) return res.send('no book exists');
+        Comment.deleteMany({ book_id: req.params.id }, (err, deletedComments) => {
+          if (err) return res.json({ error: err });
+          return res.send('delete successful');
+        });
+      });
+    });
+  
+};
